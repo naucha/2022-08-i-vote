a99a35f29be6dc0dd99588e76fda88ffcd678a5a
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-08-12 14:15:24 +0200

    Create form component to add new Poll

---
+++ components/CreateForm/CreateForm.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import homeStyles from "../../styles/Home.module.css";
+
+const CreateForm = ({ data }) => {
+  const blankFields = {
+    question: "",
+    description: "",
+    answerOne: "",
+    answerTwo: "",
+    answerThree: "",
+    creator: "",
+  };
+
+  const [formData, setFormData] = useState(blankFields);
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.id]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = {
+      title: formData.question,
+      description: formData.description,
+      options: [
+        { option: formData.answerOne },
+        { option: formData.answerTwo },
+        { option: formData.answerThree },
+      ],
+    };
+
+    console.log("data en form", data);
+    const JSONdata = JSON.stringify(data);
+    console.log("JsonDAta form", JSONdata);
+
+    const response = await fetch("api/create-vote", {
+      body: JSONdata,
+      method: "POST",
+    });
+
+    const result = await response.json();
+
+    console.log(result);
+  };
+
+  return (
+    <section>
+      <h2>Create </h2>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <fieldset className={homeStyles.card}>
+          <legend>New Poll</legend>
+          <label htmlFor="question">
+            {" "}
+            <input
+              id="question"
+              onChange={handleChange}
+              value={formData.question}
+            />
+          </label>
+
+          <textarea
+            id="description"
+            value={formData.description}
+            onChange={handleChange}
+            cols={28}
+            rows={10}
+            autoFocus={true}
+          ></textarea>
+
+          <div className="form__options--container">
+            <label htmlFor="answerOne">Answer One</label>
+            <input
+              value={formData.answerone}
+              onChange={handleChange}
+              id="answerOne"
+            />
+
+            <label htmlFor="answerTwo">Answer Two</label>
+            <input
+              value={formData.answertwo}
+              onChange={handleChange}
+              id="answerTwo"
+            />
+            <label htmlFor="answerThree">Answer Three</label>
+            <input
+              value={formData.answerthree}
+              onChange={handleChange}
+              id="answerThree"
+            />
+            <label htmlFor="creator">Creator</label>
+            <input
+              value={formData.creator}
+              onChange={handleChange}
+              id="creator"
+            />
+          </div>
+          <button type="submit">Publish</button>
+        </fieldset>
+      </form>
+    </section>
+  );
+};
+
+export default CreateForm;
