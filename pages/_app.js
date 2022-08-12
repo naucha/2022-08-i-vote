3fc68c00a42b7fdd4270e601df477f834fd132c3
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-08-12 14:17:17 +0200

    Modifing naming and path for create new Poll

--- pages/_app.js
+++ pages/_app.js
@@ -3,29 +3,32 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import Home from ".";
+import { AppWrapper } from "../store/context/appContext";
+import React from "react";
 
 const App = ({ Component, pageProps }) => {
   return (
     <>
-      <Head>
-        <title>iVote</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header>
-        <h1 className={styles.title}>iVote App!</h1>
-        <nav>
-          <Link href={`/`}>
-            <a>Home</a>
-          </Link>
-
-          <Link href={`/newpost/new`}>
-            <a>New Votation</a>
-          </Link>
-        </nav>
-      </header>
-      <main>
-        <Component {...pageProps} />
-      </main>
+      <AppWrapper>
+        <Head>
+          <title>iVote</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <header>
+          <h1 className={styles.title}>iVote App!</h1>
+          <nav>
+            <Link href={`/`}>
+              <a>Home</a>
+            </Link>
+            <Link href={`/new`}>
+              <a>Poll Maker</a>
+            </Link>
+          </nav>
+        </header>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </AppWrapper>
     </>
   );
 };
