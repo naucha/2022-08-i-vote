71a7399ffe19255021faf028697d744f1daca636
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-08-12 14:07:58 +0200

    Import item, detailItem and chart. useState to represent data on chart comp

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,16 +1,42 @@
 import Head from "next/head";
 import Link from "next/link";
-import VotationModel from "../../models/Votation";
 import dbConnect from "../../lib/dbConnectDB";
-import VotationDetailitem from "../../components/VotationDetailitem/VotationDetailitem";
+import { useState } from "react";
+import CircleChart from "../../components/CircleChart/CircleChart";
+import PollModel from "../../models/Poll";
+import PollDetailitem from "../../components/PollDetailitem/PollDetailitem";
 
-const VotationDetail = ({ votation }) => {
+const PollDetail = ({ poll }) => {
+  const { options } = poll;
+
+  const [userData, setData] = useState({
+    labels: options.map((data) => data.option),
+    datasets: [
+      {
+        label: "Users options",
+        data: options.map((data) => data.votes.length),
+        backgroundColor: [
+          "rgb(203,82,82)",
+          "rgb(69,177,223)",
+          "rgb(99,201,122)",
+          "rgb(229,224,88)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  });
+
   return (
     <>
       <Head>
         <title>Votation</title>
       </Head>
-      <VotationDetailitem votation={votation} />
+      <div style={{ width: 400 }}>
+        <PollDetailitem poll={poll} />
+        <div style={{ width: 250 }}>
+          <CircleChart chartData={userData} />
+        </div>
+      </div>
       <div>
         <Link href={"/"}>
           <a>← Back to home</a>
@@ -23,16 +49,16 @@ const VotationDetail = ({ votation }) => {
 export async function getServerSideProps(context) {
   await dbConnect();
 
-  console.log("FIND BY ID");
+  console.log("FIND BY ID ONE VOTATION");
   const {
     query: { id },
   } = context;
 
-  const votationResponse = await VotationModel.findById(id);
-  const votation = votationResponse.toObject();
-  votation._id = votationResponse._id.toString();
+  const pollResponse = await PollModel.findById(id);
+  const poll = pollResponse.toObject();
+  poll._id = pollResponse._id.toString();
 
-  return { props: { votation } };
+  return { props: { poll } };
 }
 
-export default VotationDetail;
+export default PollDetail;
