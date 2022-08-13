3b4a260919b4e1e1b985f12ddc745af1e06b0bef
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-08-13 10:58:35 +0200

    Remove comment code on index.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,10 +3,10 @@ import { useAppContext } from "../store/context/appContext";
 import dbConnect from "../lib/dbConnectDB";
 import styles from "../styles/Home.module.css";
 import PollModel from "../models/Poll";
+import mongoose from "mongoose";
 
 const Home = ({ polls }) => {
   const globalContext = useAppContext();
-
   return (
     <>
       <section className={styles.description}>
@@ -18,8 +18,8 @@ const Home = ({ polls }) => {
 
       <section>
         <ul>
-          {polls.map((poll) => (
-            <PollListItem key={poll._id} poll={poll} />
+          {polls.map((poll, index) => (
+            <PollListItem key={index} poll={poll} />
           ))}
         </ul>
       </section>
@@ -33,18 +33,8 @@ export async function getServerSideProps() {
   console.log("FIND ALL ON DB");
 
   const result = await PollModel.find({});
-  console.log("FIND DB", result);
 
   const polls = JSON.parse(JSON.stringify(result));
-
-  // const polls = JSON.parse(poll);
-  // console.log(polls);
-  // const votations = result.map((doc) => {
-
-  //   const votation = doc.toObject();
-  //   // votation._id = votation._id.toString();
-  //   return votation;
-  // });
 
   return { props: { polls } };
 }
