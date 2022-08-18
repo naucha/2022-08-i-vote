4f769e547ecf64addb2ea11b1478a5c74ea0dfb6
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-08-18 09:27:32 +0200

    Create submit-vote enpoint and update poll

---
+++ pages/api/submit-vote.js
@@ -0,0 +1,41 @@
+import dbConnect from "../../lib/dbConnectDB";
+import PollModel from "../../models/Poll";
+
+const handler = async (req, res) => {
+  const data = req.body;
+
+  const [idPollToUpdate, idOptionSelected] = data.split(",");
+
+  if (req.method === "PUT") {
+    await dbConnect();
+
+    const pollToUpdate = await PollModel.findById(idPollToUpdate);
+
+    const filterOption = pollToUpdate.options.filter(
+      (option) => option._id.toString() === idOptionSelected.toString()
+    );
+
+    const optionIndex = filterOption[0].__index;
+
+    let votesToUpdate = pollToUpdate.options[optionIndex].votes;
+    votesToUpdate = [...votesToUpdate, 1];
+
+    const updatedPoll = {
+      ...pollToUpdate.toObject(),
+
+      options: pollToUpdate.options.map((option) =>
+        option._id.toString() === idOptionSelected
+          ? { ...option.toObject(), votes: [...votesToUpdate] }
+          : option
+      ),
+    };
+
+    await PollModel.findByIdAndUpdate(idPollToUpdate, updatedPoll);
+
+    res.json({ message: `ok updated data ` });
+  } else {
+    res.json({ message: `i did nothing ` });
+  }
+};
+
+export default handler;
