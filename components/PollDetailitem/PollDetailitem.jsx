f1e0748b24ac00984df35aa96ab51909b3b01509
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-08-18 10:48:35 +0200

    PUT handler to fethc submit vote

--- components/PollDetailitem/PollDetailitem.jsx
+++ components/PollDetailitem/PollDetailitem.jsx
@@ -1,7 +1,23 @@
+import { useContext } from "react";
+import { OptionVotesContext } from "../../store/context/OptionVotesContext";
 import styles from "../../styles/Home.module.css";
 
 const PollDetailitem = ({ poll }) => {
-  const { title, description, options } = poll;
+  const { title, description, options, _id: idPoll } = poll;
+
+  const [state, setState] = useContext(OptionVotesContext);
+
+  const handlerVoted = async (index) => {
+    const optionIdToChange = options[index]._id;
+    const pollIdToChange = idPoll;
+
+    const response = await fetch("../api/submit-vote", {
+      body: [pollIdToChange, optionIdToChange],
+      method: "PUT",
+    });
+
+    const result = await response.json();
+  };
 
   return (
     <>
@@ -11,7 +27,7 @@ const PollDetailitem = ({ poll }) => {
         <ul>
           {options.map(({ option, votes }, index) => (
             <li key={index}>
-              <button>
+              <button onClick={() => handlerVoted(index)}>
                 <span>{votes.length} </span>
                 {option}
               </button>
