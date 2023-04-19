5d38767f0faf429cc43f93db8240fe263d72f028
naucha <98158987+naucha@users.noreply.github.com>
2023-04-19 18:27:04 +0200

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    screens: {
+      mobile: "440px",
+      tablet: "640px",
+      laptop: "1024px",
+      desktop: "1280px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
